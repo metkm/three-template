33b15a6f529459813e9966bea40b60b7cebed295
metkm <metinkorkmaz417@gmail.com>
2025-07-07 13:16:23 +0300

    init template 2

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
 <template>
   <TresCanvas window-size>
-    <TresPerspectiveCamera />
+    <TresPerspectiveCamera
+      :position="[5, 5, 5]"
+      :look-at="[0, 0, 0]"
+    />
+
+    <TresAxesHelper />
     <TheEffect />
   </TresCanvas>
 </template>
--- app/components/TheEffect.vue
+++ app/components/TheEffect.vue
@@ -1,17 +1,36 @@
 <script setup lang="ts">
-const pointCount = 30
+import vertexShader from '~/shader/vertex.glsl?raw'
+import fragmentShader from '~/shader/fragment.glsl?raw'
+import { Vector2 } from 'three'
+
+const { onLoop } = useRenderLoop()
+
+const pointCount = 200
 const pointPositionArray = new Float32Array(pointCount * 3)
 
 for (let index = 0; index < pointCount; index++) {
-  pointPositionArray[index * 3 + 0] = Math.random() * 4
-  pointPositionArray[index * 3 + 1] = Math.random() * 4
-  pointPositionArray[index * 3 + 2] = Math.random() * 4
+  pointPositionArray[index * 3 + 0] = Math.random() * 1
+  pointPositionArray[index * 3 + 1] = Math.random() * 1
+  pointPositionArray[index * 3 + 2] = Math.random() * 1
 }
+
+const uniforms = {
+  resolution: { value: new Vector2(window.innerWidth, window.innerHeight) },
+  time: { value: 0 },
+}
+
+onLoop(({ elapsed }) => {
+  uniforms.time.value = elapsed
+})
 </script>
 
 <template>
   <TresPoints>
     <TresBufferGeometry :position="[pointPositionArray, 3]" />
-    <TresPointsMaterial />
+    <TresShaderMaterial
+      :vertex-shader="vertexShader"
+      :fragment-shader="fragmentShader"
+      :uniforms="uniforms"
+    />
   </TresPoints>
 </template>
