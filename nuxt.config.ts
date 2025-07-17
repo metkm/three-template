2526d61aa2fb6aa164cce343576c88b97d5057ba
metkm <metinkorkmaz417@gmail.com>
2025-07-17 18:07:53 +0300

    upgrade

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/ui', '@tresjs/nuxt'],
-  ssr: false,
+  ssr: true,
   devtools: { enabled: true },
   app: {
     head: {
